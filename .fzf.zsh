a50c0f023aacb59a562fd917956adacd49bbefab
Radu Bontea <radu.bontea@nunatac.it>
2023-06-16 21:22:53 +0200

    fixed keybindings removal

--- .fzf.zsh
+++ .fzf.zsh
@@ -12,9 +12,12 @@ fi
 # ------------
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 
-bindkey -r "^T" 
-bindkey -r "^[c" 
-bindkey -r "^I" 
+bindkey -rM emacs '^T' fzf-file-widget
+bindkey -rM vicmd '^T' fzf-file-widget
+bindkey -rM viins '^T' fzf-file-widget
+bindkey -rM emacs '\ec' fzf-cd-widget
+bindkey -rM vicmd '\ec' fzf-cd-widget
+bindkey -rM viins '\ec' fzf-cd-widget
 
 # Options
 # ------------
@@ -31,32 +34,40 @@ _fzf_comprun() {
 # ------------
     cd)         find . -type d -not -path '*/.*/*' | fzf \
                                                         --preview 'tree -C {}' \
-                                                        --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                  "$@";;
+                                                        --bind '\:preview-page-up,/:preview-page-down' \
+                                                        "$@";;
     vim)        find . -type f -not -path '*/.*/*' | fzf --multi \
                                                         --preview 'bat --color=always --style=numbers {}' \
-                                                        --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                  "$@";;
+                                                        --bind '\:preview-page-up,/:preview-page-down' \
+                                                        "$@";;
 # Git
 # ------------
     gsw)        git branch -r | awk -F '/' '{print$NF}' | sort | uniq | fzf \
                                                                             --preview 'git log --stat remotes/origin/{} | bat --color=always --style=numbers' \
-                                                                            --bind '\:preview-page-up,/:preview-page-down'                                                                                                              "$@";;
+                                                                            --bind '\:preview-page-up,/:preview-page-down' \
+                                                                            "$@";;
     ga)         git diff --name-only | fzf --multi \
                                             --preview 'git diff {} | bat --color=always --style=numbers' \
-                                            --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                              "$@";;
+                                            --bind '\:preview-page-up,/:preview-page-down' \
+                                            "$@";;
     grst)       git diff --staged --name-only | fzf --multi \
                                                     --preview 'git diff --staged {} | bat --color=always --style=numbers' \
-                                                    --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                      "$@";;
+                                                    --bind '\:preview-page-up,/:preview-page-down' \
+                                                    "$@";;
     gsh)        git log --oneline --decorate | awk '{print$1}' | fzf \
                                                                     --preview 'git show {} | bat --color=always --style=numbers' \
-                                                                    --bind '\:preview-page-up,/:preview-page-down'                                                                                                                      "$@";;
+                                                                    --bind '\:preview-page-up,/:preview-page-down' \
+                                                                    "$@";;
 # Docker
 # ------------
     dxcit)      docker ps --format {{.Names}} | fzf \
                                                     --preview 'docker ps --format "Status\t{{.Status}}\nPorts\t{{.Ports}}\nMounts\t{{.Mounts}}\nImage\t{{.Image}}" --filter name={} | bat --color=always --style=numbers' \
-                                                    --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                      "$@";;
-    dcin)      docker ps -a --format {{.Names}} | fzf \
+                                                    --bind '\:preview-page-up,/:preview-page-down' \
+                                                    "$@";;
+    dcin)       docker ps -a --format {{.Names}} | fzf \
                                                     --preview 'docker ps -a --format "Status\t{{.Status}}\nPorts\t{{.Ports}}\nMounts\t{{.Mounts}}\nImage\t{{.Image}}" --filter name={} | bat --color=always --style=numbers' \
-                                                    --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                      "$@";;
-    *)          fzf                                                                                                                                                                                                                     "$@";;
+                                                    --bind '\:preview-page-up,/:preview-page-down' \
+                                                    "$@";;
+    *)  fzf "$@";;
   esac
 }
