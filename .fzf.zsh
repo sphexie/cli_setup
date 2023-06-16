e69b6895bb2d5218f3cd4f1caf496bed3a9be736
sphexie <32741815+sphexie@users.noreply.github.com>
2023-06-16 09:23:32 +0200

    Update .fzf.zsh

--- .fzf.zsh
+++ .fzf.zsh
@@ -12,10 +12,6 @@ fi
 # ------------
 source "/opt/homebrew/opt/fzf/shell/key-bindings-custom.zsh"
 
-bindkey -r "^T" 
-bindkey -r "^[c" 
-bindkey -r "^I" 
-
 # Options
 # ------------
 export FZF_DEFAULT_OPTS='--no-mouse'
@@ -27,32 +23,36 @@ _fzf_comprun() {
   shift
 
   case "$command" in
-    cd)         find . -type d | fzf \
-                                    --preview 'tree -C {}' \
-                                    --bind '\:preview-page-up,/:preview-page-down' \
-                                    "$@";;
-    vim)        find . -type f -not -path '*/.*/*' | fzf \
-                                                        --multi \
+# Basic
+# ------------
+    cd)         find . -type d -not -path '*/.*/*' | fzf \
+                                                        --preview 'tree -C {}' \
+                                                        --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                  "$@";;
+    vim)        find . -type f -not -path '*/.*/*' | fzf --multi \
                                                         --preview 'bat --color=always --style=numbers {}' \
-                                                        --bind '\:preview-page-up,/:preview-page-down' \
-                                                        "$@";;
-    gsw)        git branch -r | awk -F '/' '{print$NF}' | sort | uniq | fzf \ 
-                                                                            --preview 'git log remotes/origin/{} | bat --color=always --style=numbers' \
-                                                                            --bind '\:preview-page-up,/:preview-page-down' \
-                                                                            "$@";;
-    ga)         git diff --name-only | fzf \
-                                        --multi --preview 'git diff {} | bat --color=always --style=numbers' \
-                                        --bind '\:preview-page-up,/:preview-page-down' \
-                                        "$@";;
-    grst)       git diff --staged --name-only | fzf \
-                                                    --multi \
+                                                        --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                  "$@";;
+# Git
+# ------------
+    gsw)        git branch -r | awk -F '/' '{print$NF}' | sort | uniq | fzf \
+                                                                            --preview 'git log --stat remotes/origin/{} | bat --color=always --style=numbers' \
+                                                                            --bind '\:preview-page-up,/:preview-page-down'                                                                                                              "$@";;
+    ga)         git diff --name-only | fzf --multi \
+                                            --preview 'git diff {} | bat --color=always --style=numbers' \
+                                            --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                              "$@";;
+    grst)       git diff --staged --name-only | fzf --multi \
                                                     --preview 'git diff --staged {} | bat --color=always --style=numbers' \
-                                                    --bind '\:preview-page-up,/:preview-page-down' \
-                                                    "$@";;
+                                                    --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                      "$@";;
     gsh)        git log --oneline --decorate | awk '{print$1}' | fzf \
-                                                                    --preview 'git show {+1} | bat --color=always --style=numbers' \
-                                                                    --bind '\:preview-page-up,/:preview-page-down' \
-                                                                    "$@";;
-    *)          fzf     "$@";;
+                                                                    --preview 'git show {} | bat --color=always --style=numbers' \
+                                                                    --bind '\:preview-page-up,/:preview-page-down'                                                                                                                      "$@";;
+# Docker
+# ------------
+    dxcit)      docker ps --format {{.Names}} | fzf \
+                                                    --preview 'docker ps --format "Status\t{{.Status}}\nPorts\t{{.Ports}}\nMounts\t{{.Mounts}}\nImage\t{{.Image}}" --filter name={} | bat --color=always --style=numbers' \
+                                                    --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                      "$@";;
+    dcin)      docker ps -a --format {{.Names}} | fzf \
+                                                    --preview 'docker ps -a --format "Status\t{{.Status}}\nPorts\t{{.Ports}}\nMounts\t{{.Mounts}}\nImage\t{{.Image}}" --filter name={} | bat --color=always --style=numbers' \
+                                                    --bind '\:preview-page-up,/:preview-page-down'                                                                                                                                      "$@";;
+    *)          fzf                                                                                                                                                                                                                     "$@";;
   esac
 }
